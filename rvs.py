bbb929997685304170b039e1a400845a522657bc
ArviSantos12 <138097930+ArviSantos12@users.noreply.github.com>
2023-06-28 23:29:05 -0700

    Add files via upload

---
+++ rvs.py
@@ -0,0 +1,14 @@
+#addition
+num1=6
+num2=5
+
+sum=num1+num2
+print("THE SUM IS",sum)
+
+#subtraction
+sub_result=num1-num2
+print("SUB RESULT",sub_result)
+
+#multiplication
+mult_result=num1*num2
+print("MULT RESULT",mult_result)
